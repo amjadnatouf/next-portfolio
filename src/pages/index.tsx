a19ef6eea6f1766406be1e7117fc6f5c17ef620b
AMJAD <amjad@MacBook-Air.lan>
2023-04-19 19:36:42 +0200

    fix image size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,9 @@ import FeaturesCards from "../components/features";
 import ServicesCards from "../components/services";
 import { name, about, bio } from "../content/aboutMe";
 import avatar from "../../public/assets/avatar.webp";
-import backgroundImage from "../../public/assets/bg-home.webp";
+import image from "../../public/assets/bg-home.webp";
 import { IconArrowUpRight } from "@tabler/icons-react";
-import { createStyles, Container, rem } from "@mantine/core";
-import Image from "next/image";
+import { createStyles, Container, rem, Image } from "@mantine/core";
 
 export const metadata: Metadata = {
   title: "Welcome to My Portfolio",
@@ -88,7 +87,13 @@ export default function Home() {
             {about()}
           </p>
           <div className="flex items-center justify-start gap-[5px] sm:gap-5">
-            <Image alt={name} src={avatar.src} width={100} height={100} />
+            <Image
+              alt={name}
+              src={avatar.src}
+              width={100}
+              height={100}
+              radius={100}
+            />
             <ul className="flex items-start justify-center flex-col font-sm text-neutral-500 dark:text-neutral-400">
               <li>
                 <a
@@ -116,13 +121,7 @@ export default function Home() {
           </div>
           <p className={`my-5 max-w-[500px] ${classes.description}`}>{bio()}</p>
         </section>
-        <Image
-          src={backgroundImage.src}
-          className={classes.image}
-          alt="bg-home"
-          width={350}
-          height={350}
-        />
+        <Image src={image.src} className={classes.image} alt="" />
       </div>
       <ServicesCards />
       <FeaturesCards />
